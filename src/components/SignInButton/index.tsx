78bf5f49109d9b925648e56b17be1a6f91970591
Anderson <andersonsilvapp@gmail.com>
2021-07-15 00:06:12 -0300

    API routes: Authentication configured

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,20 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SingInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361"/>
-      Anderson Apparecido
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
     
@@ -20,6 +22,7 @@ export function SingInButton() {
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sign in with Github
